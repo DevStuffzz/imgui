57578c4cdd571ec0a25f1dd5976b5dd917657194
DevStuffzz <cb050129@gmail.com>
2025-11-15 03:35:10 -0500

    Add premake5.lua and VS project files for Nebula integration

---
+++ premake5.lua
@@ -0,0 +1,55 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "Off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp"
+	}
+
+	includedirs
+	{
+		".",
+		"../GLFW/include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "system:macosx"
+		pic "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
